2451013a8c9122cbf39dad6d859203a60a5597a5
MartyDashP <martyDashP@gmail.com>
2018-08-26 16:02:04 +0700

    - Реализовано локально хранение добавленных навыков

--- src/app/content/skills/skills.component.ts
+++ src/app/content/skills/skills.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {PersonalDataService} from '../personal-data.service';
 
+const TITLE_LOCAL_STORAGE: string = 'localSkills';
+
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
@@ -10,17 +12,29 @@ export class SkillsComponent implements OnInit {
 
   skills: string[];
   newSkill: string;
+  localSkills: any;
 
   constructor(private personalDataService: PersonalDataService) {
     this.newSkill = '';
     this.skills = this.personalDataService.getIt('skills');
+
+    if(localStorage.getItem(TITLE_LOCAL_STORAGE) !== null)
+      this.localSkills = JSON.parse(localStorage.getItem(TITLE_LOCAL_STORAGE));
+    else
+      this.localSkills = [];
   }
 
   public addSkill = (): void => {
     if (this.newSkill !== '') {
+      this.rememberSkill();
       this.skills.push(this.newSkill);
       this.newSkill = '';
     }
+  }
+
+  private rememberSkill = () => {
+    this.localSkills.push(this.newSkill);
+    localStorage.setItem(TITLE_LOCAL_STORAGE, JSON.stringify(this.localSkills));
   }
 
   ngOnInit() {
